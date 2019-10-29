988c881ddb962c60834ca1836f623a07d314bf66
Pranav Maganti <pranav.maganti@gmail.com>
2019-10-29 19:13:21 +0000

    DONE

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Alexa = require('ask-sdk-core');
+const movieQuotes = require('movie-quotes');
+const famousQuotes = require('quotes-go');
+const quoteOfTheDay = require("./quotes")
+
+
 Alexa.appid
 let skill;
 
@@ -7,6 +12,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const firebase = require("firebase");
+
+var PORT = 5000;
 
 var config = {
     apiKey: "AIzaSyCqUX77dqVfcrTyccb2BoMmwOXAvDSWMJk",
@@ -23,28 +30,63 @@ var config = {
 
 const QuoteHandler = {
     canHandle(input) {
-        return input.requestEnvelope.request.type === "InspirationalIntent";
+        return Alexa.getRequestType(input.requestEnvelope) === 'IntentRequest' ||
+        Alexa.getIntentName(input.requestEnvelope) === "QuoteIntent" 
+        || Alexa.getIntentName(input.requestEnvelope) === "AMAZON.YesIntent";
+    },
+    async handle(input) {
+        let quoteType 
+        const sessionAttributes = input.attributesManager.getSessionAttributes()
+        if (Alexa.getIntentName(input.requestEnvelope) === "AMAZON.YesIntent") {
+            quoteType = sessionAttributes.quoteType 
+        } else {
+            quoteType = capitaliseFirstLetter(Alexa.getSlotValue(input.requestEnvelope, "quoteType"))
+        }
+
+        sessionAttributes.quoteType = quoteType
+        input.attributesManager.setSessionAttributes(sessionAttributes)
+
+        
+        var quote = await getQuote(quoteType) 
+        if (quoteType == "quote of the day") {
+            quote += ". Would you like to hear another " + quoteType + "."
+        } else {
+            quote += ". Would you like to hear another " + quoteType + " quote."
+        }
+
+        var heading = quoteType + " Quote"
+        var repromptText = "You can ask for an inspirational quote, a movie quote, a famous quote or even the quote of the day."
+
+        var res = input.responseBuilder
+        .speak(quote)
+        .withSimpleCard(heading, quote)
+        .reprompt(repromptText)
+        .getResponse()
+        
+        return res 
+    }
+}
+
+const SessionEndedHandler = {
+    canHandle(input) {
+        return Alexa.getRequestType(input.requestEnvelope) === 'SessionEndedRequest' ||
+        Alexa.getRequestType(input.requestEnvelope) === 'AMAZON.StopIntent';
     },
     handle(input) {
-        console.log(input)
-        getQuote(function (quote) {
-            return input.responseBuilder
-                .speak(quote)
-                .withSimpleCard("Inspirational Quote", quote)
-                .getResponse();
-        })
-
+        return input.responseBuilder
+            .speak("Thanks for using Quote Teller. Hope to see you again soon!")
+            .getResponse();
     }
 }
 
 app.use(bodyParser.json());
 app.post('/', function (req, res) {
-
     if (!skill) {
         skill = Alexa.SkillBuilders.custom()
             .withSkillId("amzn1.ask.skill.c95db360-7a17-4118-99fa-6048917e8fda")
             .addRequestHandlers(
-                QuoteHandler
+                QuoteHandler,
+                SessionEndedHandler
             )
             .create();
     }
@@ -59,25 +101,63 @@ app.post('/', function (req, res) {
         });
 });
 
-function getQuote(callback) {
+
+async function getInspirationalQuote() {
     var path = "/quotes"
     var ref = database.ref(path)
-    ref.on('value', gotData, errData)
+    var quote = ref.once('value').then(
+        function (rawQuotes) {
+            var quotes = rawQuotes.val()
+            let randomIndex = (Math.random()*(quotes.length-1)).toFixed();
+            return Promise.resolve(quotes[randomIndex])
+        },
+        function () {
+            return Promise.reject("An error has occured. Please try again later.")
+        }
+    )
 
-    function gotData(data) {
-        var quotes = data.val()
-        if (quotes === undefined) {
-            return ("There has been an error. Please try again Later")
-        }
-        else {
-            let randomIndex = (Math.random() * (quotes.length - 1)).toFixed();
-            return (callback(quotes[randomIndex]))
-        }
+    return await quote
+        
+}
+
+app.listen(PORT)
+
+function capitaliseFirstLetter(string) {
+    return string.charAt(0).toLowerCase() + string.slice(1);
+}
+
+async function getQuote(quoteType) {
+    var quote = ""
+    if (quoteType == "inspirational") {
+        quote = await getInspirationalQuote() 
+    } else if (quoteType == "movie") {
+        quote = addHyphen(movieQuotes.random())
+    } else if (quoteType == "famous") {
+        var rawQuote = famousQuotes.getRandomQuote()
+        quote = '"' + rawQuote.text + '" - ' +  rawQuote.author.name
+    }  else if (quoteType == "quote of the day") {
+        var values = await getQuoteOfTheDay()
+        var rawQuote = values[0]
+        var author = values[1]
+        quote = '"' + rawQuote + '" - ' + author
+    } else {
+        quote = "No such quote was found. Please try again later."
     }
 
-    function errData(err) {
-        console.log("Error")
-        console.log(err)
-    }
+    return quote
+}
+
+function addHyphen(quote) {
+    var indexOfQ = quote.indexOf('"', 1);
+    var person = quote.slice(indexOfQ+1)
+    var text = quote.slice(0,indexOfQ+1)
+    var newQuote = text + " -" + person
+    return newQuote
+
+}
+
+async function getQuoteOfTheDay() {
+    var quote = await quoteOfTheDay()
+    return [quote.quote.body, quote.quote.author]
 }
 
